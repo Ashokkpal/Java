e7fa85a66c6f11bebabe55eb18efb3617acc3f61
Ashok pal <ashokpal2094@gmail.com>
2025-02-09 10:43:10 +0530

    Add files via upload

---
+++ Lecture 2/o1_demo.java
@@ -0,0 +1,9 @@
+class o1_demo{
+    public static void main(String...args){
+        System.out.print("Hello World\n"); //print and add nex line  println=print + \n
+        System.out.println("Hello World"); //print and add next line
+        System.out.print("Hello World");  //only ptint
+        
+
+    }
+}
